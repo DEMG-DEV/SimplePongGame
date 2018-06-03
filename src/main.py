03002088e6db5441a4d2ee7777b7f3c1b5e83668
David Mendez Guardado <demg@outlook.com>
2018-06-03 01:17:15 -0500

    -Definiendo el área de juego

--- src/main.py
+++ src/main.py
@@ -8,11 +8,25 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Pong Game')
 
+    try:
+        filename = os.path.join(
+            os.path.dirname(__file__),
+            'assets',
+            'graphics',
+            'background.png').replace('/', '\\')
+        background = pygame.image.load(filename)
+        background = background.convert()
+    except pygame.error as e:
+        print('Cannot load image: ', filename)
+        raise SystemExit(str(e))
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
